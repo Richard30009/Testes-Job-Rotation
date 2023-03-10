31773115ba523715f7fa2dc07725c8600e62b8bf
Richard Rodrigues <114138679+Richard30009@users.noreply.github.com>
2023-03-10 19:57:39 -0300

    Add files via upload

---
+++ teste03.py
@@ -0,0 +1,15 @@
+#3) Descubra a lógica e complete o próximo elemento:
+
+
+
+#a) 1, 3, 5, 7, ___ / 9 A sequencia esta de numeros impares
+
+#b) 2, 4, 8, 16, 32, 64, ____ / 128 A sequencia e o dobro do anterior
+
+#c) 0, 1, 4, 9, 16, 25, 36, ____ / 49 A sequencia e baseada na soma de numeros impares Ex: 0+1 = 1, 1+3 = 4, 4+5 = 9 e assim por diante
+
+#d) 4, 16, 36, 64, ____ / 100 A sequencia e baseada no quadrado perfeito de numeros pares.
+
+#e) 1, 1, 2, 3, 5, 8, ____ / 13 A sequencia de Fibonacci
+
+#f) 2,10, 12, 16, 17, 18, 19, ____ / 200 A sequencia e baseada na letra inicial do numero o D - essa foi dificil pois foge da logica das anteriores.
